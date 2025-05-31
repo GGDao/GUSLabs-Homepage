bd08b729a763670d1018e9db8c4a00b5b09fc245
CodeRad <coderaddesign@gmail.com>
2025-05-30 18:37:51 -0700

    Remove outdated text links and update copyright in footer

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -5,7 +5,7 @@ interface FooterProps {}
 const Footer: React.FC<FooterProps> = ({}) => {
   return (
     <footer className=" relative flex flex-col space-y-10 justify-center p-10 bg-[rgb(10,10,10)]">
-      <nav className="flex justify-center flex-wrap gap-6 text-gray-500 font-medium">
+      {/* <nav className="flex justify-center flex-wrap gap-6 text-gray-500 font-medium">
         <a className="hover:text-gray-900" href="#">
           Home
         </a>
@@ -18,7 +18,7 @@ const Footer: React.FC<FooterProps> = ({}) => {
         <a className="hover:text-gray-900" href="https://discord.gg/gustakes" target="_blank">
           Contact Us
         </a>
-      </nav>
+      </nav> */}
       <div className="flex justify-center space-x-5">
         <a href="https://discord.gg/gustakes" target="_blank" rel="noopener noreferrer">
           <img
@@ -43,7 +43,7 @@ const Footer: React.FC<FooterProps> = ({}) => {
         </a>
       </div>
       <p className="text-center text-gray-700 font-medium">
-        &copy; 2024 GUSLabs. All rights reserved.
+        &copy; 2021-2025 GUSLabs. All rights reserved.
       </p>
     </footer>
   );
