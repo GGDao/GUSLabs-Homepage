9e3b2e3644c5aabcb900b30c8d30373ec7a3f381
CodeRad <coderaddesign@gmail.com>
2025-05-30 18:40:23 -0700

    Remove extraneous components from main App display

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import Games from "./sections/Games/Games";
 import Hero from "./sections/Hero/Hero";
-import JoinCommunity from "./sections/JoinCommunity/JoinCommunity";
 import Team from "./sections/Team/Team";
 import Features from "./sections/Features/Features";
-import PrimaryCTA from "./sections/PrimaryCTA/PrimaryCTA";
 import Footer from "./sections/Footer/Footer";
 
 function App() {
@@ -12,8 +9,6 @@ function App() {
       <Hero />
       <Features />
       <Team />
-      <PrimaryCTA />
-      <JoinCommunity />
       <Footer />
     </div>
   );
